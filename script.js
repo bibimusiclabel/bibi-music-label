e528d5ecd992a60b8d7fb4661210de35a31c6416
bibimusiclabel <bibimusiclabel@gmail.com>
2026-07-29 00:23:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
+function setLanguage(language) {
 
+
+const greek = document.querySelectorAll(".lang-el");
+
+const english = document.querySelectorAll(".lang-en");
+
+
+
+if(language === "en"){
+
+
+greek.forEach(function(element){
+
+element.style.display = "none";
+
+});
+
+
+english.forEach(function(element){
+
+element.style.display = "inline";
+
+});
+
+
+}
+
+else{
+
+
+english.forEach(function(element){
+
+element.style.display = "none";
+
+});
+
+
+greek.forEach(function(element){
+
+element.style.display = "inline";
+
+});
+
+
+}
+
+
+}
